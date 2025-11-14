769517892c9cd6c1ffca9317c28d70eadd573269
Yu Hang <yuhangmak123@gmail.com>
2025-11-13 23:18:22 -0500

    fix: Putting in raw file

--- test-activity/src/App.js
+++ test-activity/src/App.js
@@ -1,13 +1,11 @@
 
 import './App.css';
 
-const logoBase64 = 'PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAxMDAgMTAwIj4KICA8Y2lyY2xlIGN4PSI1MCIgY3k9IjUwIiByPSI0MCIgc3R5bGU9ImZpbGw6I2ZmYzAwMDsiIC8+Cjwvc3ZnPg==';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={`data:image/svg+xml;base64,${logoBase64}`} className="App-logo" alt="logo" />
+        <img src={`data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAxMDAgMTAwIj4KICA8Y2lyY2xlIGN4PSI1MCIgY3k9IjUwIiByPSI0MCIgc3R5bGU9ImZpbGw6I2ZmYzAwMDsiIC8+Cjwvc3ZnPg==`} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
