6caf30358f9dc837137d42a17d77401bd18cb8e4
Yu Hang <yuhangmak123@gmail.com>
2025-11-13 22:37:51 -0500

    fix: index.js

--- test-activity/src/index.js
+++ test-activity/src/index.js
@@ -4,14 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Wait for DOM to load
+document.addEventListener('DOMContentLoaded', () => {
+  const rootElement = document.getElementById('root');
+  if (rootElement) {
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    // optional: start web vitals
+    reportWebVitals();
+  } else {
+    console.error('Root element not found!');
+  }
+});
