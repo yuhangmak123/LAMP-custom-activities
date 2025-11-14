501650797c7d83dbf5399136ffc148a6d6fed7a8
Yu Hang <yuhangmak123@gmail.com>
2025-11-13 22:42:50 -0500

    fix: Icon not loading

--- test-activity/src/App.js
+++ test-activity/src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+// Convert logo to base64
+const logoBase64 = `data:image/svg+xml;base64,${btoa(
+  require('./logo.svg').default
+)}`;
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logoBase64} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
