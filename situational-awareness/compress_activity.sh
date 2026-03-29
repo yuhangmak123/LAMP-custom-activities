80a2bb4c587ee3570596022920bad8bbbdde048e
Yu Hang <yuhangmak123@gmail.com>
2026-03-29 18:26:45 -0400

    Testing new compress_activity.sh

--- situational-awareness/compress_activity.sh
+++ situational-awareness/compress_activity.sh
@@ -2,10 +2,16 @@
 # npx --yes --package inline-source-cli inline-source --attribute "" --compress false --root ./build ./build/index.html > dist.html
 # openssl base64 -in dist.html -out dist.html.b64
 
-name=$(basename "$(pwd)")  # or set manually if needed
+name=$(basename "$(pwd)")
+
+mkdir -p "../dist/in" "../dist/out"
 
 npx --yes --package inline-source-cli inline-source \
   --attribute "" \
   --compress false \
   --root ./dist \
-  ./dist/index.html | tee "../dist/in/$name.html" | openssl base64 > "../dist/out/$name.html.b64"
+  ./dist/index.html > "../dist/in/$name.html"
+
+openssl base64 -A \
+  -in "../dist/in/$name.html" \
+  -out "../dist/out/$name.html.b64"
