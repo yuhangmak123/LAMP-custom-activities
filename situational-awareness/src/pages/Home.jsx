f7f09b698d9810bead72be6b1c44a09345255ada
Yu Hang <yuhangmak123@gmail.com>
2026-04-17 21:46:10 -0400

    fix: Wrong property name for event handler

--- situational-awareness/src/pages/Home.jsx
+++ situational-awareness/src/pages/Home.jsx
@@ -18,7 +18,7 @@ export default function Home() {
 
     return (
         <Box>
-            <IconButton onClickonClick={handleClickBack}>
+            <IconButton onClick={handleClickBack}>
                 <ArrowBackIcon />
             </IconButton>
             <Box>
